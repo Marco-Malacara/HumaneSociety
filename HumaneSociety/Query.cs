a3c8cd5061609c247e1ffb94e80dbd774f0593b0
Trenton Daniels <trentondaniels95@gmail.com>
2018-08-21 10:02:24 -0500

    added GetPendingAdoptions method

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -9,5 +9,11 @@ namespace HumaneSociety
     public static class Query
     {
         
+        public static IEnumerable<Adoption> GetPendingAdoptions()
+        {
+            HumaneSocietyDataContext db = new HumaneSocietyDataContext();
+            var pendingAdoptions = db.Adoptions.Select(Animal => Animal).Where(Animal => Animal.ApprovalStatus == "pending");
+            return pendingAdoptions;
+        }
     }
 }
