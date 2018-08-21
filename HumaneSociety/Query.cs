c33b416db1f5994681b67a4acf352a8041b258e4
alynielson <nielsonaly@gmail.com>
2018-08-21 09:56:19 -0500

    initial UpdateAdoption function

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace HumaneSociety
 {
-    public static class Query
+    public static class Query 
     {
-        
+        public void UpdateAdoption(bool isApproved, Adoption adoption)
+        {
+            HumaneSocietyDataContext db = new HumaneSocietyDataContext();
+            switch (isApproved)
+            {
+                case true:
+                    db.Adoptions.SingleOrDefault(a => adoption.AdoptionId == a.AdoptionId);
+
+                    break;
+                case false:
+                    break;
+            }
+        }
     }
 }
