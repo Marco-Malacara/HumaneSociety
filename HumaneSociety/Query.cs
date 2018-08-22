8350e339702d89ac72a07bfc9a9d0f2600c4a7f3
Trenton Daniels <trentondaniels95@gmail.com>
2018-08-22 14:59:02 -0500

    fixed merge conflict

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -390,7 +390,7 @@ namespace HumaneSociety
         private static void DeleteEmployee(Employee employee)
         {
             HumaneSocietyDataContext db = new HumaneSocietyDataContext();
-            var employeeToDelete = db.Employees.SingleOrDefault(user => user.EmployeeId == employee.EmployeeId);
+            var employeeToDelete = db.Employees.SingleOrDefault(user => user.EmployeeNumber == employee.EmployeeNumber);
             if (employeeToDelete != null)
             {
                 db.Employees.DeleteOnSubmit(employeeToDelete);
