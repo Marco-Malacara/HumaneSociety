ca77c71a775951abbaf3895123fe3a25901ba3b7
Marco-Malacara <marco.malacara7@gmail.com>
2018-08-23 10:14:03 -0500

    added sql file SeededDB for review

---
+++ SeededDB.sql
@@ -0,0 +1,27 @@
+INSERT INTO [HumanSociety].[dbo].[Species] VALUES ('tiger');
+INSERT INTO [HumanSociety].[dbo].[Species] VALUES ('cat');
+INSERT INTO [HumanSociety].[dbo].[Species] VALUES ('dog');
+INSERT INTO [HumanSociety].[dbo].[Species] VALUES ('sheep');
+INSERT INTO [HumanSociety].[dbo].[Species] VALUES ('duck');
+
+INSERT INTO [HumanSociety].[dbo].[Animals] VALUES ('sally', 1, 2000, 19, 1, 'friendly', 1, 0, 'female', 'available', 1);
+INSERT INTO [HumanSociety].[dbo].[Animals] VALUES ('luci', 2, 28, 2000, 3, 'aggressive', 0, 0, 'male', 'available', 2);
+INSERT INTO [HumanSociety].[dbo].[Animals] VALUES ('buster', 3, 70, 2, 2, 'very friendly', 1, 1, 'male', 'available', 3);
+INSERT INTO [HumanSociety].[dbo].[Animals] VALUES ('baaa', 4, 300, 13, 5, 'passive', 0, 1, 'undecided', 'available', 2);
+INSERT INTO [HumanSociety].[dbo].[Animals] VALUES ('roger', 5, 4000, 150, 6, 'skiddish', 0, 0, 'male', 'available', 1);
+
+INSERT INTO [HumanSociety].[dbo].[Rooms] VALUES (101, 1);
+INSERT INTO [HumanSociety].[dbo].[Rooms] VALUES (666, 2);
+INSERT INTO [HumanSociety].[dbo].[Rooms] VALUES (103, 3);
+INSERT INTO [HumanSociety].[dbo].[Rooms] VALUES (104, 4);
+INSERT INTO [HumanSociety].[dbo].[Rooms] VALUES (105, 5);
+INSERT INTO [HumanSociety].[dbo].[Rooms] VALUES (106, null);
+INSERT INTO [HumanSociety].[dbo].[Rooms] VALUES (107, null);
+INSERT INTO [HumanSociety].[dbo].[Rooms] VALUES (108, null);
+INSERT INTO [HumanSociety].[dbo].[Rooms] VALUES (109, null);
+INSERT INTO [HumanSociety].[dbo].[Rooms] VALUES (110, null);
+
+
+INSERT INTO [HumanSociety].[dbo].[Employees] VALUES('marco', 'malacara', 'marco', 'rattpack', 1, 'marco@gmail.com');
+INSERT INTO [HumanSociety].[dbo].[Employees] VALUES('aly', 'nielson', 'aly', 'catlover', 2, 'aly@gmail.com');
+INSERT INTO [HumanSociety].[dbo].[Employees] VALUES('trent', 'daniels', 'trent', 'ligma', 3, 'trent@gmail.com');
